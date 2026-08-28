bff6edc024ce1d692c82d248f63991f6100c58cc
sargoo <goon_aguea@hotmail.com>
2026-08-28 12:31:53 -0300

    creacion de clase

---
+++ src/Empleado.java
@@ -0,0 +1,20 @@
+public abstract class   Empleado {
+    private String nombre;
+    private double salario;
+
+    public Empleado(String nombre, double Salario){
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public double getSalario(){
+        return this.salario;
+    }
+    public void setSalario (double salario){
+        this.salario = salario;
+    }
+
+    public abstract double calcularPago();
+}
